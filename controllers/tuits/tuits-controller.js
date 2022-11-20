9770c63e46645e1598125edb4b580045c57768ea
zhuorangao <zhuorgao@gmail.com>
2022-11-20 17:57:37 -0500

    update

--- controllers/tuits/tuits-controller.js
+++ controllers/tuits/tuits-controller.js
@@ -1,20 +1,37 @@
 import posts from "./tuits.js";
 let tuits = posts;
 
-export default (app) => {
-    app.post('/api/tuits', createTuit);
-    app.get('/api/tuits', findTuits);
-    app.put('/api/tuits/:tid', updateTuit);
-    app.delete('/api/tuits/:tid', deleteTuit);
+
+
+
+const templateTuit = {
+
+    "topic": "Space",
+    "time": "2h",
+    "liked": false,
+    "replies": 0,
+    "retuits": 0,
+    "likes": 0,
+    "dislikes": 0
 }
 
 
-
 const createTuit = (req, res) => {
-    const newTuit = req.body;
-    newTuit._id = (new Date()).getTime()+'';
-    newTuit.likes = 0;
-    newTuit.liked = false;
+    // const newTuit = {
+    //     ...req.body,
+    //     ...templateTuit
+    // };
+      const newTuit = req.body;
+     newTuit._id = (new Date()).getTime()+'';
+     newTuit.likes = 0;
+     newTuit.liked = false;
+     newTuit.dislikes = 0;
+     newTuit.replies = 0;
+     newTuit.retuits = 0;
+     newTuit.handle = "@nasa";
+     newTuit.username = "NASA";
+     newTuit.image = "nasa.jpg";
+     newTuit.time = "2h";
     tuits.push(newTuit);
     res.json(newTuit);
 }
@@ -46,3 +63,9 @@ const updateTuit = (req, res) => {
 const findTuits = (req, res) =>
     res.json(tuits);
 
+export default (app) => {
+    app.post('/api/tuits', createTuit);
+    app.get('/api/tuits', findTuits);
+    app.put('/api/tuits/:tid', updateTuit);
+    app.delete('/api/tuits/:tid', deleteTuit);
+}
